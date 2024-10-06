d59051ee60f223b121a64a8c9859aaadbb839a8d
Aziz-souli <aziz.souli@enicar.ucar.tn>
2024-10-06 22:43:02 +0100

    Stcok price Checker

---
+++ db.js
@@ -0,0 +1,15 @@
+
+require('dotenv').config({ path: 'sample.env' });
+const mongoose = require('mongoose')
+
+const connectToDB = async () => {
+   
+    try {
+        await mongoose.connect(process.env.DB);
+        console.log("DB is connected");
+    } catch (err) {
+        console.error({ error: err });
+    }
+};
+
+module.exports = connectToDB
